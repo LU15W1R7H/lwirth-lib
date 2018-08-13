caa9c0fccac20144a5e621ba206f96d5586fb077
Luis Wirth <lwirth2000@gmail.com>
2018-08-13 15:43:20 +0200

    added Eigen

--- lwirth-testing/Main.cpp
+++ lwirth-testing/Main.cpp
@@ -6,11 +6,13 @@ using namespace lw;
 
 #include <iostream>
 
-
+#include <eigen/Dense>
 
 int main()
 {
-	std::cout << "Test" << '\n';
+	Eigen::Vector3f vec = { 2.f, 2.f, 1.f };
+	std::cout << vec.norm() << '\n';
+	std::cout << vec << '\n';
 
 	std::cin.get();
 }
